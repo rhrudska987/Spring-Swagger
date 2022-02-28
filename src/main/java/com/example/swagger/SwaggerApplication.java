9681773b3b2026c3c0b455fbe8e4068aca80805a
KyungNam Ko <rhrudska987@naver.com>
2022-02-28 18:29:34 +0900

    2022.02.28 Study(need to study more) this code doesn't run because of some reason... and i don't know how to solve this problem first, I use Stringfox Boot Starter dependency. But It doesn't match my IntelliJ version second, I use Springfox Swagger UI and Springfox Swagger2 dependencies, but I don't know how to use it. the tutor only user first dependency...

---
+++ src/main/java/com/example/swagger/SwaggerApplication.java
@@ -0,0 +1,22 @@
+package com.example.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
+//@EnableAsync
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+
+}
